30811ac5b6d627af025708de3e4f6a9952f9ff85
Taranchuk <nfhfyxer@gmail.com>
2021-06-26 14:51:43 +0300

    update

---
+++ 1.2/Source/MedievalOverhaulAncientDead/HarmonyPatches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MedievalOverhaulAncientDead
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyContainer
+    {
+        public static Harmony harmony;
+        static HarmonyContainer()
+        {
+            harmony = new Harmony("MedievalOverhaulAncientDead.Mod");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(HediffSet), "CalculatePain")]
+    public class Patch_CalculatePain
+    {
+        private static void Postfix(HediffSet __instance, ref float __result)
+        {
+            if (__instance.pawn.def == MO_DefOf.DankPyon_AncientDeadRace)
+            {
+                __result = 0f;
+            }
+        }
+    }
+}
